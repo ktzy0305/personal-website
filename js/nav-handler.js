d4a141d0ff9a57dd87a665f539f6197061f6efa6
Kevin Toh <kevintoh0305@gmail.com>
2019-08-30 15:33:25 +0800

    Site Revamp

---
+++ js/nav-handler.js
@@ -0,0 +1,34 @@
+
+$(document).ready(()=>
+{
+  var lastId;
+  var topMenu = $("#nav-menu");
+  var topMenuHeight = topMenu.outerHeight()+15;
+  var menuItems =  topMenu.find("a");
+  var scrollItems = menuItems.map(function(){
+    var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+  });
+
+  $(window).scroll(function(){
+    // Get container scroll position
+    var fromTop = $(this).scrollTop()+topMenuHeight;
+    
+    // Get id of current scroll item
+    var cur = scrollItems.map(function(){
+      if ($(this).offset().top < fromTop)
+        return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+    
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+          .parent().removeClass("active")
+          .end().filter("[href='#"+id+"']").parent().addClass("active");
+    }                   
+ });
+});
--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-(function(){
-    var loading = 0;
-    var id = setInterval(frame, 64);
-    function frame(){
-        if(loading == 100){
-            clearInterval(id);
-            window.open("aboutme.html", "_self")
-        } else{
-            loading = loading + 2.5;
-            if(loading == 90){
-                document.getElementById("load").style.animation = "fadeout 1s ease";
+$(document).ready(function () {
+    new WOW().init();
+    $(".owl-carousel").owlCarousel(
+        {
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false,
+                    loop: false,
+                },
+                768: {
+                    items: 2,
+                    nav: false,
+                    loop: false,
+                },
+                1000: {
+                    items: 3,
+                    nav: false,
+                    loop: false,
+                }
             }
         }
-    }
-})();
+    );
+});
