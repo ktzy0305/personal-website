ded440c1e87dbdce3c973bc8eb1948c45c9d0b1f
Kevin Toh <kevintoh0305@gmail.com>
2024-04-08 00:38:45 +0800

    New Blog On GenAI

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,75 +1,75 @@
-import Date from "@/app/components/date";
-import { getPostData } from "@/lib/posts";
-import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
-import Image from "next/image";
-import utilStyles from "@/styles/utils.module.css";
-import "@/app/blog/styles.css";
+import Date from '@/app/components/date';
+import { getPostData } from '@/lib/posts';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+import Image from 'next/image';
+import utilStyles from '@/styles/utils.module.css';
+import '@/app/blog/styles.css';
 
 interface PostData {
-  title: string,
-  date: string,
-  contentHtml: string,
+    title: string;
+    date: string;
+    contentHtml: string;
 }
 
 type Params = {
-  id: string
-}
+    id: string;
+};
 
 type Props = {
-  params: Params
-}
+    params: Params;
+};
 
 export async function generateMetadata({ params }: Props) {
-  const postData: PostData = await getPostData(params.id);
+    const postData: PostData = await getPostData(params.id);
 
-  return {
-    title: postData.title,
-  }
+    return {
+        title: postData.title,
+    };
 }
 
 export default async function Post({ params }: Props) {
-  const postData: PostData = await getPostData(params.id);
+    const postData: PostData = await getPostData(params.id);
 
-  return (
-    <div className="min-h-screen px-4 sm:px-16 md:px-24 lg:px-32 xl:px-64 lg:-mt-20 lg:pt-32">
-      {/* Back to Blog Link */}
-      <Link href="/blog">
-        <div className="flex flex-row mr-auto w-48 rounded-lg py-3 px-3 content-center hover:bg-white hover:text-black">
-          <FaArrowLeft className="mr-3 w-6 h-6" />
-          <span>Back to Blog</span>
+    return (
+        <div className="min-h-screen px-4 sm:px-16 md:px-24 lg:px-32 xl:px-64 lg:-mt-20 lg:pt-32">
+            {/* Back to Blog Link */}
+            <Link href="/blog">
+                <div className="flex flex-row mr-auto w-48 rounded-lg py-3 px-3 content-center hover:bg-white hover:text-black">
+                    <FaArrowLeft className="mr-3 w-6 h-6" />
+                    <span>Back to Blog</span>
+                </div>
+            </Link>
+            <article className="px-3 py-6">
+                {/* Post Title */}
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+
+                {/* Author and Date  */}
+                <div className="flex flex-row mb-3">
+                    <Image
+                        priority
+                        className="rounded-full mr-4"
+                        src="/images/profile/kevin.jpg"
+                        height={64}
+                        width={64}
+                        alt={'Kevin'}
+                    />
+                    <div className="flex flex-col my-auto">
+                        {/* Post Author */}
+                        <div>Kevin Toh</div>
+                        {/* Post Date */}
+                        <div className={utilStyles.lightText}>
+                            <Date dateString={postData.date} />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Post Content */}
+
+                <div
+                    dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+                />
+            </article>
         </div>
-      </Link>
-      <article className="px-3 py-6">
-        {/* Post Title */}
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-
-        {/* Author and Date  */}
-        <div className="flex flex-row mb-3">
-          <Image
-            priority
-            className='rounded-full mr-4'
-            src="/images/profile/kevin.jpg"
-            height={64}
-            width={64}
-            alt={"Kevin"}
-          />
-          <div className="flex flex-col my-auto">
-            {/* Post Author */}
-            <div>Kevin Toh</div>
-            {/* Post Date */}
-            <div className={utilStyles.lightText}>
-              <Date dateString={postData.date} />
-            </div>
-          </div>
-        </div>
-
-        {/* Post Content */}
-        
-        <div
-          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
-        />
-      </article>
-    </div>
-  );
+    );
 }
--- lib/posts.ts
+++ lib/posts.ts
@@ -23,7 +23,11 @@ export function getSortedPostsData() {
         // Combine the data with the id
         return {
             id,
-            ...(matterResult.data as { date: string, title: string, description: string }),
+            ...(matterResult.data as {
+                date: string;
+                title: string;
+                description: string;
+            }),
         };
     });
 
@@ -34,7 +38,7 @@ export function getSortedPostsData() {
         } else {
             return -1;
         }
-    })
+    });
 }
 
 export function getAllPostIds() {
@@ -43,7 +47,7 @@ export function getAllPostIds() {
     return fileNames.map((fileName) => {
         return {
             params: {
-                id : fileName.replace(/\.md$/, ''),
+                id: fileName.replace(/\.md$/, ''),
             },
         };
     });
@@ -66,6 +70,6 @@ export async function getPostData(id: string) {
     return {
         id,
         contentHtml,
-        ...(matterResult.data as { date: string, title: string }),
+        ...(matterResult.data as { date: string; title: string }),
     };
 }
