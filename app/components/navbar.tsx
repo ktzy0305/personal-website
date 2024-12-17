db36b61b6f1a30636b8ccb936aed0f270a404289
Kevin Toh <kevintoh0305@gmail.com>
2024-12-17 09:52:31 +0800

    Theme and navigation enhancements

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "../../styles/navigation.module.css";
 import ToggleSwitch from "./switch";
 import { useTheme } from "../contexts/ThemeContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const NavigationBar = (): JSX.Element => {
   const { isDarkMode } = useTheme() ?? {};
@@ -11,6 +12,8 @@ const NavigationBar = (): JSX.Element => {
   const isActive = (path: string) => pathname === path;
   const isBlogPost = () => pathname.startsWith("/blog");
   const isNotes = () => pathname.startsWith("/notes");
+
+  const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
 
   useEffect(() => {
     // Update CSS variables when the theme changes
@@ -37,29 +40,82 @@ const NavigationBar = (): JSX.Element => {
     }
   }, [isDarkMode]);
 
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <div className="lg:sticky lg:top-0 z-40">
+    <div className="lg:sticky lg:top-0 shadow-lg z-40">
       <nav
         className={`${
           isDarkMode ? "bg-deepblue-100" : "bg-light-100"
-        } navbar flex justify-end items-center gap-8 sm:gap-4 py-6 px-6 sm:px-8 lg:px-24 xl:px-32`}
+        } navbar flex flex-wrap justify-between items-center gap-8 sm:gap-4 py-4 px-6 sm:px-8 lg:px-24 xl:px-32`}
       >
-        <div
-          className={`${styles.navItem} ${isActive("/") ? styles.active : ""}`}
-        >
-          <Link href="/">Home</Link>
+        <div className="flex-shrink-0 flex items-center gap-8 sm:gap-4">
+          <Image
+            src="/images/logo/KT.png" 
+            alt={""}
+            width={52}
+            height={52}       
+          />
+          <div className="font-bold text-xl">
+            <Link href="/">
+              Kevin Toh
+            </Link>
+          </div>
         </div>
-        <div
-          className={`${styles.navItem} ${
-            isActive("/blog") || isBlogPost() ? styles.active : ""
-          }`}
+
+        <button 
+          data-collapse-toggle="navbar-menu" 
+          type="button" 
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
+          aria-controls="navbar-menu" 
+          aria-expanded={isMenuOpen}
+          onClick={toggleMenu}
         >
-          <Link href="/blog">Blog</Link>
+          <span className="sr-only">Open main menu</span>
+          <svg 
+            className="w-5 h-5" 
+            aria-hidden="true" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 17 14"
+          >
+            <path 
+              stroke="currentColor" 
+              stroke-linecap="round" 
+              stroke-linejoin="round" 
+              stroke-width="2" 
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        
+        <div className={`${isMenuOpen ? "" : "hidden"} w-full md:block md:w-auto`} id="navbar-menu">
+          <div className="flex flex-col md:flex-row md:items-center gap-12 sm:gap-4">
+            <div
+              className={`
+                ${styles.navItem} ${isActive("/") ? styles.active : ""
+              }`}
+            >
+              <Link href="/">Home</Link>
+            </div>
+            <div
+              className={`${styles.navItem} ${
+                (isActive("/blog") || isBlogPost()) ? styles.active : ""
+              } md:ml-3`}
+            >
+              <Link href="/blog">Blog</Link>
+            </div>
+            {/* <div className={`${styles.navItem} ${isActive('/notes') || isNotes() ? styles.active : ""} md:ml-3`}>
+              <Link href="/notes">Notes</Link>
+            </div> */}
+            {/* <div className={`${styles.navItem} ${isActive('/publications') || isPublications() ? styles.active : ""}`}>
+              <Link href="/publications">Publications</Link>
+            </div> */}
+            <ToggleSwitch />
+          </div>
         </div>
-        {/* <div className={`${styles.navItem} ${isActive('/notes') || isNotes() ? styles.active : ""}`}>
-          <Link href="/notes">Notes</Link>
-        </div> */}
-        <ToggleSwitch />
       </nav>
     </div>
   );
